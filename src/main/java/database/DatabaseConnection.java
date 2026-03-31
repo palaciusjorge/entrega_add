a314960836c5f304b644b75f1d310b5de15275b5
palaciusjorge <palaciusjorge@gmail.com>
2026-03-31 10:04:17 +0200

    Estructura de carpetas

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,17 @@
+package database;
+
+import java.sql.Connection;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/entrega_add";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public static Connection conectar(){
+        try {
+            return java.sql.DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (java.sql.SQLException e) {
+            throw new RuntimeException("Error de conexion: " + e.getMessage(), e);
+        }
+    }
+}
